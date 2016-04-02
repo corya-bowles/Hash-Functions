822e58297a20b46bcc4f67142ec7a9340acc7aea
juanvallejo <juan.vallejo.12@cnu.edu>
2016-04-01 22:51:18 -0400

    add frequency stats (part 2)

--- src/HashFunctions.java
+++ src/HashFunctions.java
@@ -68,13 +68,45 @@ public class HashFunctions {
 		binStringArr[arrNum] = binStringArr[arrNum].substring(0, arrPos) + 
 			newPosVal + binStringArr[arrNum].substring(arrPos + 1);
 
-		byte[] fByteArr = new byte[a.length];
-		for(int i = 0; i < a.length; i++) {
-			fByteArr[i] = (byte)(Byte.parseByte(binStringArr[i], 2));
+		try {
+
+			byte[] fByteArr = new byte[a.length];
+			for(int i = 0; i < a.length; i++) {
+				fByteArr[i] = (byte)(Byte.parseByte("+" + (binStringArr[i]), 2));
+			}
+
+			return fByteArr;
+
+		} catch(NumberFormatException e) {
+			// e.printStackTrace();
+			// System.out.println("NumberFormatException flipping bit " + position);
 		}
 
-		return fByteArr;
+		return a;
 
+	}
+
+	/**
+	 * Takes the original message's 32 bit hash and an updated
+	 * message's 32 bit hash, incrementing count for each index
+	 * value passed as the last argument. Assumes msgA and msgB
+	 * are 32 bit messages and bitDiffCount is a 32 int array
+	 * @param msgA 			byte array containing 32-bit hash of a message
+	 * @param msgB 			byte array containing 32-bit hash of a message
+	 * @param bitDiffCount 	int array containing bit counts for each of 32 bits
+	 */
+	public static int[] updateBitChangeCount(byte[] msgA, byte[] msgB, int[] bitDiffCount) {
+
+		String strA = HashFunctions.getBinaryByteString(msgA);
+		String strB = HashFunctions.getBinaryByteString(msgB);
+
+		for(int i = 0; i < strA.length(); i++) {
+			if(strA.charAt(i) != strB.charAt(i)) {
+				bitDiffCount[i] = (bitDiffCount[i] + 1);
+			}
+		}
+
+		return bitDiffCount;
 	}
 
 	public static String[] getBinaryByteArrayString(byte[] array) {
@@ -132,7 +164,7 @@ public class HashFunctions {
 		System.out.println(HashFunctions.getBinaryByteString(test1));
 
 		// test flip method
-		byte[] test2 = HashFunctions.flip(test1, 5);
+		byte[] test2 = HashFunctions.flip(test1, 0);
 		System.out.println(HashFunctions.getBinaryByteString(test2));
 
 		// test diff method
@@ -140,25 +172,64 @@ public class HashFunctions {
 		System.out.println(HashFunctions.getBinaryByteString(test3));
 		System.out.println(HashFunctions.diff(test1, test3));
 
-		System.out.println();
-
-		//Trying out part two of the project
+		System.out.println("Begin part 2");
 
 		//SHA-1 is one of the most widely used hash functions
 		//Creates an instance of a SHA-1 mdSUM to be used in this class or something like that, right?  
-		try { md = MessageDigest.getInstance("SHA-1");//32 bytes
+		try { 
+			md = MessageDigest.getInstance("SHA-1");//32 bytes
 		}
-		catch(NoSuchAlgorithmException e) { e.printStackTrace();
+		catch(NoSuchAlgorithmException e) { 
+			e.printStackTrace();
 		}
 
-		 
-
+		// 8-byte arbitrary message
+		byte[] arbM = (new String("Test Msg")).getBytes();
 		
+		// first four bytes of arbMHash
+		byte[] arbMHash4 = H(md.digest(arbM));
 
-		byte[] arbM = {0x01, 0x01, 0x01, 0x01};
-		System.out.println(HashFunctions.getBinaryByteString(arbM));
-		byte[] hashBytes = md.digest(arbM);
-		System.out.println(HashFunctions.getBinaryByteString(H(hashBytes)));
+		// keep track of each bit's change
+		int[] msgBitChangeCount = new int[32];
+
+		int totalDiffs = 0;
+		int lowestDiff = 0;
+		int highestDiff = 0;
+		int msgCount = 0;
+		double avgDiffCount = 0;
+
+		for(int i = 0; i < arbM.length * 8; i++) {
+			byte[] newMsgHash4 = H(md.digest(HashFunctions.flip(arbM, i)));
+			int diffCount = HashFunctions.diff(newMsgHash4, arbMHash4);
+			
+			if(diffCount < lowestDiff) {
+				lowestDiff = diffCount;
+			}
+
+			if(diffCount > highestDiff) {
+				highestDiff = diffCount;
+			}
+
+			HashFunctions.updateBitChangeCount(arbMHash4, newMsgHash4, msgBitChangeCount);
+
+			totalDiffs += diffCount;
+			msgCount++;
+		}
+
+		avgDiffCount = totalDiffs / msgCount;
+
+		System.out.println("Message count = " + msgCount);
+		System.out.println("Lowest diff count = " + lowestDiff);
+		System.out.println("Highest diff count = " + highestDiff);
+		System.out.println("Average diff count = " + avgDiffCount);
+		System.out.println("Total diffs = " + totalDiffs);
+
+		System.out.println("Change frequency for each bit:");
+
+		for(int i = 0; i < msgBitChangeCount.length; i++) {
+			System.out.println("> " + msgBitChangeCount[i]);
+		}
+
 	}
 
 }
